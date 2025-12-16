8de0e4a68660f56e75015738240f20fbdee6b123
evalin2 <eva.lindenschmidt@mng.ch>
2025-12-16 07:53:05 +0100

    Refactor database schema with new tables

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -1,13 +1,41 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(250) NOT NULL UNIQUE,
-    password VARCHAR(250) NOT NULL
+DROP TABLE nutzer;
+DROP TABLE tennisplatz;
+DROP TABLE wartunsarbeiter;
+DROP TABLE buchung;
+
+
+CREATE TABLE nutzer (
+    nid INT AUTO_INCREMENT PRIMARY KEY,
+    vorname VARCHAR(250),
+    nachname VARCHAR(250),
+    geburtsdatum DATE,
+    email VARCHAR(100)
 );
 
-CREATE TABLE todos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    content VARCHAR(100),
-    due DATETIME,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE wartungsarbeiter (
+    wid INT AUTO_INCREMENT PRIMARY KEY,
+    vorname VARCHAR(250),
+    nachname VARCHAR(250),
+    geburtsdatum DATE
+);
+
+CREATE TABLE tennisplatz (
+    tid INT AUTO_INCREMENT PRIMARY KEY,
+    belag VARCHAR(100),
+    tennisanlage VARCHAR(250),
+    platznummer VARCHAR(10),
+    standort VARCHAR(250),
+    wid INT NOT NULL,
+    datum_der_wartung DATE,
+    FOREIGN KEY (wid) REFERENCES wartungsarbeiter(wid)
+);
+
+CREATE TABLE buchung (
+    buchungsnummer INT AUTO_INCREMENT PRIMARY KEY,
+    nid INT NOT NULL,
+    tid INT NOT NULL,
+    spieldatum DATE,
+    spiellaenge CHAR(2),
+    FOREIGN KEY (nid) REFERENCES nutzer(nid),
+    FOREIGN KEY (tid) REFERENCES tennisplatz(tid)
 );
