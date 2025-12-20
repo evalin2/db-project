9e272969e7f22995a46e405b2e119f49426a3563
evalin2 <eva.lindenschmidt@mng.ch>
2025-12-20 10:43:12 +0100

    k

--- flask_app.py
+++ flask_app.py
@@ -109,4 +109,10 @@ def logout():
 @app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
-    pass
+    return render_template("index.html")
+
+@app.route("/users", methods=["GET"])
+@login_required
+def users():
+    users = db_read("SELECT username FROM users ORDER BY username", ())
+    return render_template("users.html", users=users)
