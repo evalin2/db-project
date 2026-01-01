55e2c440148ecee741bee47157491f0f93dd98ba
evalin2 <eva.lindenschmidt@mng.ch>
2026-01-01 19:09:53 +0100

    k

--- flask_app.py
+++ flask_app.py
@@ -125,7 +125,7 @@ def stornieren():
 def sbestätigt():
     return render_template("sbestätigt.html")
 
-@app.route("/vlogin")
+@app.route("/verwaltung/login")
 @login_required
-def vlogin():
-    return render_template("vlogin.html")
+def login():
+    return render_template("login.html")
