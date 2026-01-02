d12c7147f7abc673f2b1f595712497e21aae1653
evalin2 <eva.lindenschmidt@mng.ch>
2026-01-02 11:54:17 +0100

    k

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -1,7 +1,7 @@
-DROP TABLE nutzer;
+DROP TABLE buchung;
 DROP TABLE tennisplatz;
-DROP TABLE wartunsarbeiter;
-DROP TABLE buchung; 
+DROP TABLE wartungsarbeiter;
+DROP TABLE nutzer; 
 
 
 CREATE TABLE nutzer (
@@ -22,8 +22,8 @@ CREATE TABLE wartungsarbeiter (
 CREATE TABLE tennisplatz (
     tid INT AUTO_INCREMENT PRIMARY KEY,
     tennisanlage VARCHAR(250),
-    platznummer VARCHAR(10),
-    belag VARCHAR(100),
+    platznummer INT,
+    belag VARCHAR(50),
     wid INT NOT NULL,
     datum_der_wartung DATE,
     FOREIGN KEY (wid) REFERENCES wartungsarbeiter(wid)
@@ -41,21 +41,21 @@ CREATE TABLE buchung (
 );
 
 INSERT INTO nutzer (vorname, nachname, geburtsdatum, email) VALUES
-('Max', 'Müller', 01.01.2000, 'max.müller@email.ch'),
-('Anna', 'Schwarz', 12.12.1999, 'anna.schwarz@email.ch');
+('Max', 'Müller', '2000-01-01', 'max.mueller@email.ch'),
+('Anna', 'Schwarz', '1999-12-12', 'anna.schwarz@email.ch');
 
 INSERT INTO wartungsarbeiter (vorname, nachname, geburtsdatum) VALUES
-('Sonja', 'Sonne', 28.02.1985),
-('Anton', 'Alt', 07.05.1965),
-('Martin', 'Meier', 01.10.1992);
+('Sonja', 'Sonne', '1985-02-28'),
+('Anton', 'Alt', '1965-05-07'),
+('Martin', 'Meier', '1992-10-01');
 
 INSERT INTO tennisplatz (tennisanlage, platznummer, belag, wid, datum_der_wartung) VALUES
-('Tennis Club Klauberg', 1, 'hart', 1, '10.11.2025').
-('Tennis Club Klauberg', 2, 'hart', 1, '10.11.2025'),
-('Tennis Club Klauberg', 3, 'hart', 1, '10.11.2025'),
-('Tennis Club Klauberg', 4, 'hart', 1, '10.11.2025'),
-('Tanner Tennisclub', 2, 'Sand', 2, '10.11.2025');
+('Tennis Club Klauberg', 1, 'hart', 1, '2025-11-10'),
+('Tennis Club Klauberg', 2, 'hart', 1, '2025-11-10'),
+('Tennis Club Klauberg', 3, 'hart', 1, '2025-11-10'),
+('Tennis Club Klauberg', 4, 'hart', 1, '2025-11-10'),
+('Tanner Tennisclub', 2, 'Sand', 2, '2025-11-10');
 
-INSERT INTO buchung (nid, tid, spieldatum, spielbeginn, spielende);
-(1, 1, 16.12.2025, 14:00, 15:00),
-(2, 5, 18., 12:00, 13:00);
+INSERT INTO buchung (nid, tid, spieldatum, spielbeginn, spielende) VALUES
+(1, 1, '2025-12-16', '14:00:00', '15:00:00'),
+(2, 5, '2025-12-18', '12:00:00', '13:00:00');
