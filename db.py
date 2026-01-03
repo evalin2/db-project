7061492f9c9321f403fa831479ac028cd7ab91b6
evalin2 <eva.lindenschmidt@mng.ch>
2026-01-03 19:00:15 +0100

    k

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ from mysql.connector import pooling
 
 # Load .env variables
 load_dotenv()
+
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
@@ -13,6 +14,7 @@ DB_CONFIG = {
 
 # Init db
 pool = pooling.MySQLConnectionPool(pool_name="pool", pool_size=5, **DB_CONFIG)
+
 def get_conn():
     return pool.get_connection()
 
@@ -20,9 +22,10 @@ def get_conn():
 def db_read(sql, params=None, single=False):
     conn = get_conn()
     try:
-        cur = conn.cursor(dictionary=True)
+        # buffered=True verhindert "Unread result found" Fehler
+        cur = conn.cursor(dictionary=True, buffered=True)
         cur.execute(sql, params or ())
-
+        
         if single:
             # liefert EIN Dict oder None
             row = cur.fetchone()
@@ -33,14 +36,12 @@ def db_read(sql, params=None, single=False):
             rows = cur.fetchall()
             print("db_read(single=False) ->", rows)  # DEBUG
             return rows
-
     finally:
         try:
             cur.close()
         except:
             pass
         conn.close()
-
 
 def db_write(sql, params=None):
     conn = get_conn()
